2ed270cf9cb06d3322c3192346efcb0b3ac1ce9a
MikolajDawidowski <dawmster@gmail.com>
2017-05-24 13:01:06 +0200

    Adding my_C_Function in C sharp file

--- XamarinMacApp/XamarinMacApp/AppDelegate.cs
+++ XamarinMacApp/XamarinMacApp/AppDelegate.cs
@@ -1,18 +1,28 @@
 ﻿using AppKit;
 using Foundation;
 
+
+using System.Runtime.InteropServices;
+
 namespace XamarinMacApp
 {
+    
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
-    {
+     {
+        
         public AppDelegate()
         {
         }
 
+		[DllImport("__Internal")]
+		static extern int my_C_Function();
         public override void DidFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
+
+            int myretval = my_C_Function();
+
         }
 
         public override void WillTerminate(NSNotification notification)
